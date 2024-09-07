86038144313e8f1d2ae5856a219d46b1693e1641
Payam3006 <payam3006@gmail.com>
2024-09-07 15:13:56 +0330

    last

--- src/pages/game1.js
+++ src/pages/game1.js
@@ -11,54 +11,64 @@ import { Game1Box } from "../components/game1-box";
 // export default Home;
 
 export const Game1 = () => {
-  const [score, setScore] = useState(0);
+  const setActiveBoxes = (boxesNum, activeBoxesNum) => {
+    const boxesArray = [];
+    for (let i = 1; i <= boxesNum; i++) {
+      boxesArray.push(0);
+    }
 
-  const [active, setActive] = useState(1);
+    while (
+      boxesArray.filter((el) => {
+        return el !== 0;
+      }).length < activeBoxesNum
+    ) {
+      let randomIndx = Math.ceil(Math.random() * boxesNum);
+      boxesArray[randomIndx - 1] = 1;
+    }
+
+    // console.log(boxesArray);
+    return boxesArray;
+  };
+
+  const [score, setScore] = useState(0);
+  const [boxesArray, setBoxesArray] = useState(Array(5).fill(0));
 
   const sumScore = (boxScore, negetiveScore, clickRight) => {
     clickRight ? setScore(score + boxScore) : setScore(score + negetiveScore);
     console.log(boxScore, negetiveScore);
   };
 
-  // console.log("document mounted", new Date().getSeconds());
+  console.log("document mounted", new Date().getSeconds());
 
-  useEffect(() => {
-    console.log("document mounted", new Date().getSeconds());
-    setInterval(() => {
-      setActive(Math.ceil(Math.random() * 8));
-      // console.log(Math.ceil(Math.random() * 8));
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   console.log("document mounted", new Date().getSeconds());
+  //   setInterval(() => {
+  //     setBoxesArray(() => {
+  //       const newArray = setActiveBoxes(5, 2);
+  //       return newArray;
+  //     });
+  //     console.log(boxesArray);
+  //   }, 2000);
+  // }, []);
 
   return (
     <>
       <div className="container">
         <div className="score-board">score: {score}</div>
         <div className="box-container">
-          <Game1Box
-            name="box1"
-            callBackFunction={sumScore}
-            boxScore={3}
-            activate={false}
-            timeOut={0}
-            timeIn={0}
-          />
-          <Game1Box
-            name="box2"
-            callBackFunction={sumScore}
-            boxScore={3}
-            activate={active === 2}
-            timeOut={0}
-            timeIn={0}
-          />
-          <Game1Box
-            name="box3"
-            callBackFunction={sumScore}
-            boxScore={3}
-            activate={true}
-            timeOut={0}
-            timeIn={0}
-          />
+          {boxesArray.map((element, index) => {
+            return (
+              <Game1Box
+                key={index}
+                name={`box${index + 1}`}
+                callBackFunction={sumScore}
+                boxScore={3}
+                activate={element === 1}
+                timeOut={0}
+                timeIn={0}
+              />
+            );
+          })}
         </div>
       </div>
     </>
